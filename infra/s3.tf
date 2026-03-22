b016ad5926b7f4f273e08db80c46abd54d8c114d
mallett002 <mallett002@gmail.com>
2026-03-22 07:38:04 -0500

    upload new app to s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -41,10 +41,41 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
   })
 }
 
-# upload index.html to bucket
-resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.site-bucket.id
-  key          = "index.html"
-  source       = "../src/index.html"
-  content_type = "text/html"
+# upload app to s3
+locals {
+  dist_files = fileset("${path.module}/../lead-forge-spa/dist", "**")
+
+  content_types = {
+    ".html" = "text/html"
+    ".js"   = "application/javascript"
+    ".css"  = "text/css"
+    ".svg"  = "image/svg+xml"
+    ".png"  = "image/png"
+  }
+}
+
+resource "aws_s3_object" "lead-forge-website-files" {
+  for_each = { for file in local.dist_files : file => file }
+
+  bucket = aws_s3_bucket.site-bucket.id
+  key    = each.value
+  source = "${path.module}/../lead-forge-spa/dist/${each.value}"
+
+  etag = filemd5("${path.module}/../lead-forge-spa/dist/${each.value}")
+
+  content_type = lookup(
+    local.content_types,
+    regex("\\.[^.]+$", each.value),
+    "application/octet-stream"
+  )
+
+  cache_control = (
+    each.value == "index.html"
+    ? "no-cache, no-store, must-revalidate"
+    : "public, max-age=31536000, immutable"
+  )
+
+  lifecycle {
+    ignore_changes = [etag]
+  }
 }
