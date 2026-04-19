baea65da78b43d1bc470799bbb5d622abe21b077
williammallett <37602780+mallett002@users.noreply.github.com>
2026-04-19 06:56:10 -0500

    Prevent emails from going to spam by using domain identity with DKIM (#7)
    * notes
    
    * success using domain w/ dkim; failed to send: unverified/sandbox
    
    * infra notes on ses
    
    * notes & cleanup
    
    * notes
    
    * rm

--- infra/ses.tf
+++ infra/ses.tf
@@ -5,16 +5,48 @@
 # sends email
 
 
-# allow lambda to send email via ses (exec role)
-resource "aws_ses_email_identity" "ses-sender" {
-  email = "mallett002@gmail.com"
-}
-
 resource "aws_ses_template" "lead-forge-verification" {
   name    = "lead-forge-verification"
   subject = "Verify your email"
-
   html = file("${path.module}/templates/verification.html")
-
   text = "Hello {{name}}, verify here: {{verificationLink}}"
+}
+
+# configuation set for tracking and metrics
+resource "aws_ses_configuration_set" "config_set" {
+  name = "ses_config_set"
+  reputation_metrics_enabled = true # Amazon CloudWatch metric. The default value is false
+  sending_enabled = true # email sending is enabled or disabled for the configuration set. The default value is true.
+
+  delivery_options {
+    tls_policy = "Require"  #If the value is Optional, messages can be delivered in plain text if a TLS connection can't be established. 
+  }
+}
+
+# domain identity for farmtotablenearme.com
+resource "aws_ses_domain_identity" "ses_domain" {
+  domain = "farmtotablenearme.com"
+}
+
+# DKIM: "Domain Keys for Identified Mail"
+# Proves that email sent from your domain really comes from your domain and wasn't tampered with
+# mail providers (gmail) look up your public key in DNS to verify
+resource "aws_ses_domain_dkim" "ses_dkim" {
+  domain = aws_ses_domain_identity.ses_domain.domain
+}
+
+# DKIM selectors: CNAME records that point to AWS-hosted public keys used to verify SES-signed emails
+resource "aws_route53_record" "ses_dkim_record" {
+  count   = length(aws_ses_domain_dkim.ses_dkim.dkim_tokens)
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${aws_ses_domain_dkim.ses_dkim.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${aws_ses_domain_dkim.ses_dkim.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+resource "aws_ses_domain_identity_verification" "domain_identity_verification" {
+  domain = aws_ses_domain_identity.ses_domain.id
+
+  depends_on = [aws_route53_record.ses_dkim_record]
 }
--- notes.md
+++ notes.md
@@ -38,4 +38,12 @@
 - Add basic logging/metrics
 
 
+### ses
+- identity: email or domain identities (the who mail gets sent from)
+    - domain better and can use DKIM (domain keys)
+    - probably need this to prevent mail going to spam
+- domain with DKIM can auto verify this idenity to send
+- can use templates to interpolate variables
+- In sandbox mode, need to request production access.
+    - They'll send you an email you need to respond to, describing how you will use SES
 
